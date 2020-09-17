31ca5b25a82199a79e4074aef1f4f5c57862fc49
Daniel <dotonesimple@gmail.com>
2020-09-17 09:32:16 +0200

    [WIP] Created initial gulp task

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const { src, dest } = require("gulp");
+const through2 = require('through2');
+const Path =  require('path');
+const yaml =  require('js-yaml');
+function defaultTask(cb) {
+    // place code for your default task here
+    cb();
+    return src('input.scss')
+            .pipe(through2.obj(function(file, _, cb) {
+                if (file.isBuffer()) {
+                    file.contents = Buffer.from(file.contents);
+                  
+                    file.path = Path.basename(file.path, ".scss") + ".yml"; //change path ext to .yml
+                  
+                  
+                    file.contents = Buffer.from(yaml.safeDump({test: "hi", test2: [{name:"farbe", color: "red"}]}, {
+                        'styles': {
+                        '!!null': 'canonical' // dump null as ~
+                        },
+                        'sortKeys': true        // sort object keys
+                    })); 
+                }
+                cb(null, file);
+              }))
+            .pipe(dest('output/'));
+}
+  
+  exports.default = defaultTask
