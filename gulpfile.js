f7474b15c97526003f8e24df72af187004a02887
Daniel <dotonesimple@gmail.com>
2020-09-17 12:42:49 +0200

    Formatted gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,29 @@
 const { src, dest } = require("gulp");
 const through2 = require('through2');
-const Path =  require('path');
-const yaml =  require('js-yaml');
+const Path = require('path');
+const yaml = require('js-yaml');
 function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-    return src('input.scss')
-            .pipe(through2.obj(function(file, _, cb) {
-                if (file.isBuffer()) {
-                    file.contents = Buffer.from(file.contents);
-                  
-                    file.path = Path.basename(file.path, ".scss") + ".yml"; //change path ext to .yml
-                  
-                  
-                    file.contents = Buffer.from(yaml.safeDump({test: "hi", test2: [{name:"farbe", color: "red"}]}, {
-                        'styles': {
-                        '!!null': 'canonical' // dump null as ~
-                        },
-                        'sortKeys': true        // sort object keys
-                    })); 
-                }
-                cb(null, file);
-              }))
-            .pipe(dest('output/'));
+  // place code for your default task here
+  cb();
+  return src('input.scss')
+    .pipe(through2.obj(function (file, _, cb) {
+      if (file.isBuffer()) {
+        file.contents = Buffer.from(file.contents);
+
+        file.path = Path.basename(file.path, ".scss") + ".yml"; //change path ext to .yml
+
+
+        //dump 
+        file.contents = Buffer.from(yaml.safeDump({ test: "hi", test2: [{ name: "farbe", color: "red" }] }, {
+          'styles': {
+            '!!null': 'canonical' // dump null as ~
+          },
+          'sortKeys': true        // sort object keys
+        }));
+      }
+      cb(null, file);
+    }))
+    .pipe(dest('output/'));
 }
-  
-  exports.default = defaultTask
+
+exports.default = defaultTask
