5d1473f25910d7de0ef0c79d4ba10b059b4068a0
Daniel <dotonesimple@gmail.com>
2020-09-18 19:26:16 +0200

    Added option parsing

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const readline = require('readline');
 function defaultTask(cb) {
   // place code for your default task here
   cb();
-  return src('input.scss')
+  return src('example.scss')
     .pipe(through2.obj(function (file, _, cb) {
       if (file.isBuffer()) {
 
@@ -16,7 +16,7 @@ function defaultTask(cb) {
         //split scss file into lines, only lines including octoberyml: {} will be converted to a configuration option
         const lines = file.contents.toString().split(/(?:\r\n|\r|\n)/g);
 
-        var commentPattern = / {0,}\$(.{1,})\: {0,}(.{1,}) {0,}; {0,}\/{2} {0,}octoberyml: {0,}\{ {0,}(.{0,}) {0,}\}/i;
+        var commentPattern = / {0,}\$(.{1,})\: {0,}(.{1,}) {0,}; {0,}\/{2} {0,}octoberyml: {0,}(\{ {0,}.{0,} {0,}\})/i;
 
         let variables = {};
 
@@ -24,11 +24,15 @@ function defaultTask(cb) {
           var match = line.match(commentPattern);
           if(match != null) {
             const variableName = match[1];
+            const sanatizedVariableName = variableName.replace("-", "_");
             const defaultValue = match[2];
-            const options = match[3];
-            const optionsObject = {};
-            variables[variableName] = {test: "hi"};
-            console.log(variables);
+            let options;
+            try {
+              options = looseJsonParse(match[3]);  
+            } catch(e) {
+              throw new Error("invalid options string: " + options);
+            }
+            variables[sanatizedVariableName] = {default: defaultValue, assetVar: variableName, ...options};
           } 
         }
 
@@ -37,14 +41,18 @@ function defaultTask(cb) {
           'styles': {
             '!!null': 'canonical' // dump null as ~
           },
-          'sortKeys': true        // sort object keys
         }));
-        console.log("OUTPUT");
+        console.log("\n### OUTPUT ###\n");
         console.log(file.contents.toString());
       }
       cb(null, file);
     }))
     .pipe(dest('output/'));
+}
+
+//from https://developer.mozilla.org/
+function looseJsonParse(obj){
+  return Function('"use strict";return (' + obj + ')')(); //@reviewer, don't use Function? alternative would be JSON.parse()
 }
 
 exports.default = defaultTask
