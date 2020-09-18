d72b2b86f935e6cbc70c6ab89f94822c111ce06b
Daniel <dotonesimple@gmail.com>
2020-09-18 21:29:59 +0200

    Removed callback

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -2,16 +2,13 @@ const { src, dest, series } = require("gulp");
 const scssToOctober = require('../src/scss-to-october');
 const mergeOctoberYaml = require("../src/merge-october-yaml");
 
-function buildExampleYmlFromScss(cb) {
-  // place code for your default task here
-  cb();
+function buildExampleYmlFromScss() {
   return src('example.scss')
     .pipe(scssToOctober())
     .pipe(dest('output/'));
 }
 
-function mergeOctoberYamlTask(cb) {
-  cb();
+function mergeOctoberYamlTask() {
   return src("october.yml")
     .pipe(mergeOctoberYaml("output/example.yml"))
     .pipe(dest("output/"));
